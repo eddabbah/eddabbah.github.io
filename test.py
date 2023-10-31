e6bf2dcdba51221fd98afc410d8d94616cfea441
eddabbah mohamed <eddabbah@gmail.com>
2023-11-01 00:05:06 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-
+import requests
+from ast import literal_eval
+import json
+url="https://api.open-meteo.com/v1/meteofrance?latitude=52.52&longitude=13.41&hourly=temperature_2m&start_date=2023-10-30&end_date=2023-11-03"
+response=requests.get(url).content.decode('utf-8')
+mydic=literal_eval(response)
+print(mydic["hourly"]["temperature_2m"])
